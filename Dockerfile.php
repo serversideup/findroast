eee08e2a871935f75dffdd12815c0ce6138a9587
Dan Pastori <dan@521dimensions.com>
2026-02-21 07:33:08 -0600

    Update environment configuration and Docker setup for MariaDB integration. Change database connection settings in .env.example, update composer dependencies, and enhance Docker Compose files for development and production environments. Add health checks for services and update Traefik configuration for improved routing and security.

--- Dockerfile.php
+++ Dockerfile.php
@@ -4,7 +4,7 @@
 
 # Learn more about the Server Side Up PHP Docker Images at:
 # https://serversideup.net/open-source/docker-php/
-FROM serversideup/php:8.4-fpm-nginx-alpine AS base
+FROM serversideup/php:8.5-fpm-nginx AS base
 
 ## Uncomment if you need to install additional PHP extensions
 USER root
@@ -30,7 +30,7 @@ RUN update-ca-certificates
 
 # Set the user ID and group ID for www-data
 RUN docker-php-serversideup-set-id www-data $USER_ID:$GROUP_ID  && \
-    docker-php-serversideup-set-file-permissions --owner $USER_ID:$GROUP_ID --service nginx
+    docker-php-serversideup-set-file-permissions --owner $USER_ID:$GROUP_ID
 
 # Drop privileges back to www-data    
 USER www-data
@@ -41,12 +41,7 @@ USER www-data
 FROM base AS ci
 
 # Sometimes CI images need to run as root
-# so we set the ROOT user and configure
-# the PHP-FPM pool to run as www-data
 USER root
-RUN echo "" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \ 
-    echo "user = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \
-    echo "group = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf
 
 ############################################
 # Production Image
